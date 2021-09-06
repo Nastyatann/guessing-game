3bc66fd7ec02e99e749029ba11b60b477b78e092
nastyatann <anastasia.kuz.84@gmail.com>
2021-09-06 19:34:14 +0300

    feat: add guessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        let res = Math.round((this.max + this.min) / 2);
+        return res;
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
-
+ 
 module.exports = GuessingGame;
